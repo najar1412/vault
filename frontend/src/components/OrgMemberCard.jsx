1e4a78f6c85d2946f53398d6dd59330c2d6439fe
najar1412 <rory.jarrel@gmail.com>
2025-02-09 11:16:00 +0000

    move loading to state

--- frontend/src/components/OrgMemberCard.jsx
+++ frontend/src/components/OrgMemberCard.jsx
@@ -1,28 +1,29 @@
 import { Group, Text, UnstyledButton, Image, Box, Avatar } from "@mantine/core";
-import { useState } from "react";
-import { Link } from "react-router";
+
+import { useSiteStore } from "../Store";
 import { API } from "../constant";
 import { getToken } from "../helpers";
+
 import deleteIcon from "../assets/icons/delete_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg";
 import editIcon from "../assets/icons/edit_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg";
 import profileIcon from "../assets/icons/face_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg";
 
 export const OrgMemberCard = ({ member, orgId }) => {
-  /* const { setUserVaults, userVaults, selectedOrg, setSelectedOrg } =
-    useSiteStore(); */
-  const [isLoading, setIsLoading] = useState(false);
+  const { setIsLoading } = useSiteStore();
 
   const handleDeleteVault = async () => {
+    console.log("handleDeleteVault");
     // delete owner from organ
     //await fetchDeleteOrg();
   };
+
   const removeMemberFromOrg = async (member, orgId) => {
+    setIsLoading(true);
     try {
       const response = await fetch(`${API}/organisations/${orgId}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
-          // set the auth token to the user's jwt
           Authorization: `Bearer ${getToken()}`,
         },
         body: JSON.stringify({
@@ -36,7 +37,7 @@ export const OrgMemberCard = ({ member, orgId }) => {
     } catch (error) {
       console.error(error);
     } finally {
-      // setIsLoading(false);
+      setIsLoading(false);
     }
   };
 
